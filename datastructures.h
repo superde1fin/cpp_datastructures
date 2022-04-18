53d33fb0356272057f019622b9b91e5b4e6a4be7
Vasiliy <kopolokrbrgr@gmail.com>
2022-04-18 18:48:39 -0400

    Corrected return types of class stack and queue functions

--- datastructures.h
+++ datastructures.h
@@ -9,8 +9,8 @@ class SQNode{
         ~SQNode();
     friend class Stack;
     friend class Queue;
-    friend ostream& operator << (ostream& os, Node& nd);
-    friend string toString(Node& nd);
+    friend ostream& operator << (ostream& os, SQNode& nd);
+    friend string toString(SQNode& nd);
     };
     
 class Queue{
@@ -23,7 +23,7 @@ class Queue{
         int length();
         Queue();
         ~Queue();
-        SQNode* deQ();
+        node_value_type deQ();
         SQNode* enQ(node_value_type);
     friend ostream& operator << (ostream& os, Queue& q);
     friend string toString(Queue& q);
@@ -39,9 +39,9 @@ class Stack{
         ~Stack();
         bool isEmpty();
         int length();
-        int push(node_value_type);
-        int pop();
-        int peek(){;
+        SQNode* push(node_value_type);
+        node_value_type pop();
+        node_value_type peek(){;
         
     friend ostream& operator << (ostream& os, Stack& stk);
     friend string toString(Stack& stk);
