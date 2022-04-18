210673869e7e48b7219878c483a4a9aabfc9560d
Vasiliy <kopolokrbrgr@gmail.com>
2022-04-18 18:30:32 -0400

    Started the Queue class

--- queue.hpp
+++ queue.hpp
@@ -1,1 +1,20 @@
+#include "datastructures.h"
+#include "SQNode.hpp"
 
+Queue::Queue(){
+    Queue::front = nullptr;
+    Queue::rear = nullptr;
+    Queue::size = 0;
+    }
+
+Queue::~Queue(){
+    Queue::front = nullptr;
+    Queue::rear = nullptr;
+    Queue::size = NULL;
+    }
+    
+bool Queue::isEmpty(){return Queue::size == 0;}
+int Queue::length(){return Queue::size}
+SQNode* Queue::deQ(){
+    if Queue::isEmpty(){}
+    }
